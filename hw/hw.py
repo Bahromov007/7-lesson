2a487ff57707f9374ab5f5e6194cd6a177da85f3
Bahromov Abdulhakim <aa0064473@gmail.com>
2021-12-14 16:21:46 +0500

    added 7 lesson

---
+++ hw/hw.py
@@ -0,0 +1,10 @@
+# num1 = int(input('enter the 1st number: '))
+# num2 = int(input('enter the 2nd number: '))
+# if num1 > num2: print(f'{num2} - min number')
+# else: print(f'{num1} - min number')
+
+# num = int(input('enter the number: '))
+# if num > 0: print(num ** 2)
+# elif num < 0: print(num ** (1/3))
+# else: print('num is 0')
+
---
+++ hw/index.py
@@ -0,0 +1,34 @@
+name1 = 'Zafar'
+name2 = 'Abdulkahkim'
+
+names = ['Zafar', 'Abdulkhakim', 'Bobur', 'Zakir', 'Shukhrat', 'Aziz', 'Atabek', 'Amalmalik']
+ages = [21, 15, 17, 20]
+# names.append('Elbek')
+# names.append(23)
+# names.append('True')
+# names.pop(0)
+# print(names.pop(0))
+# print(names.pop(0))
+# print(names.pop(0))
+# print(names.pop(0))
+# print(names.pop(0))
+# # names.insert(0, 'Elbek')
+# print(len(names))
+# names.remove('Bobur')
+names.reverse()
+# print(names[0:2])
+# print(names[0:7:2])
+# print(names[0::2])
+# print(names[::2])
+# print(names[::-1])
+# names = names[::-1]
+
+# # print(names[0])
+# # print(names[1])
+# print(names[-1])
+# print(names[3])
+print(names)
+
+# ages.remove(21)
+# ages.remove(29)
+# print(ages)
